39bed245d45f8fc625ef60e3ba6601a20b32dc5f
PAULO <adrian.paulo.vaz@gmail.com>
2024-09-23 02:50:16 -0600

    api

---
+++ APIS/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+@app.route("/")
+def root():
+    return "Home"
+
+    # GET --> obtener información
+    # POST --> crear información
+    # PUT --> actualizar información
+    # DELETE --> eliminar información
+@app.route("/users/<user_id>")
+def get_user(user_id):
+    user = {"id": user_id, 'name':'test', 'telefono':'123456'}
+    #/users/2654?query=query_test
+    query = request.args.get("query")
+    if query:
+        user["query"] = query
+    return jsonify(user), 200
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    data["status"]='user created'
+    return jsonify(data), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
